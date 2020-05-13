465f9ae0584e6e34040777e3f9a6ed0ebf6d44c4
behnam.yaghoubi <behnam.yaghoubi@partdp.ai>
2020-05-13 11:49:01 +0430

    refactor todoModel

--- models/TodoModel.js
+++ models/TodoModel.js
@@ -1,22 +1,30 @@
-const {sequelize, DataTypes} = require('../config/database');
-
-const Todo = sequelize.define('Todo',{
-    id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER,
-        // field:'id'
+const { sequelize, DataTypes } = require("../config/database");
+const Todo = sequelize.define("Todo", {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+    // field:'id'
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "user",
+      key: "id",
     },
-    userId:{
-        
-    },
-    todoName:{
-        type: DataTypes.STRING,
-    },
-    done:{
-        type: DataTypes.ENUM,
-        values:['done', 'doing', 'incomplete'],
-        defaultValue: 'incomplete'
-    }
-})
+  },
+  todoName: {
+    type: DataTypes.STRING,
+    unique: true,
+  },
+  Condition: {
+    type: DataTypes.ENUM,
+    values: ["done", "doing", "incomplete"],
+    defaultValue: "incomplete",
+  },
+});
+Todo.associate = (models) => {
+  User.hasMany(models.Todo, { foreignKey: "userId" });
+};
+module.exports = Todo;
