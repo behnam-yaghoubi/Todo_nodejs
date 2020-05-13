9c67158c62e70bfbc049f3c61d9f6bebce1d767c
behnam.yaghoubi <behnam.yaghoubi@partdp.ai>
2020-05-13 12:02:53 +0430

    refactor userModel

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,52 +1,57 @@
-const {sequelize, DataTypes} = require('../config/database');
-const bcrypt = require('bcryptjs')
-    
-const User = sequelize.define('user', {
+const { sequelize, DataTypes } = require("../config/database");
+const bcrypt = require("bcryptjs");
+
+const User = sequelize.define(
+  "user",
+  {
     id: {
-        // allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER,
-        // field:'id'
+      // allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      // field:'id'
     },
     userName: {
-        type: DataTypes.STRING,
-        // field:'userName'
+      type: DataTypes.STRING,
+      // field:'userName'
     },
     email: {
-        type: DataTypes.STRING,
-        validate:{
-            isEmail:true
-        }
-        // field:'email'
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+      },
+      // field:'email'
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull:false
-        // field:'password'
-    }
-},{
+      type: DataTypes.STRING,
+      allowNull: false,
+      // field:'password'
+    },
+  },
+  {
     // timestamps:true,
-    freezeTableName: true
+    freezeTableName: true,
+  }
+);
+
+User.beforeCreate(async (user, option) => {
+  // crypt user password
+  ///////////////////////////////////////////// sol 1
+  // return bcrypt.hash(user.password , 10)
+  //     .then(hash =>{
+  //         user.password = hash
+  //     })
+  //     .catch(err => {
+  //         throw new Error();
+  //     })
+  ///////////////////////////////////////////// sol 2
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  /////////////////////////////////////////////
 });
 
-User.beforeCreate(async(user, option)=>{ // crypt user password
-    ///////////////////////////////////////////// sol 1
-    // return bcrypt.hash(user.password , 10)
-    //     .then(hash =>{
-    //         user.password = hash
-    //     })
-    //     .catch(err => {
-    //         throw new Error();
-    //     })
-    ///////////////////////////////////////////// sol 2
-    const salt  = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt)
-    /////////////////////////////////////////////
-})
-
-User.prototype.validPassword = async(password)=>{
-    return await bcrypt.compare(password, this.password)
-}
+User.prototype.validPassword = async (password) => {
+  return await bcrypt.compare(password, this.password);
+};
 
 module.exports = User;
