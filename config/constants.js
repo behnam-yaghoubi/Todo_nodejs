711e2a6aa7a7141d0db2922530b9204b4f727907
behnam.yaghoubi <behnam.yaghoubi@partdp.ai>
2020-05-13 15:58:04 +0430

    create constant values

---
+++ config/constants.js
@@ -0,0 +1,8 @@
+export const JWT_SECRET = "behnamBackendDeveloper";
+export const DATABASE = {
+  DB_NAME: "test",
+  DB_USERNAME: "postgres",
+  DB_PASSWORD: "behnam",
+  DB_URL: "localhost",
+  DB_DIALECT: "postgres",
+};
