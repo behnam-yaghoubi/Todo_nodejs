b8f67e4bf6e71ddd4606f01a4cd88af3e6d014a4
behnam.yaghoubi <behnam.yaghoubi@partdp.ai>
2020-05-13 11:47:32 +0430

    refactor

--- routes/Todo.js
+++ routes/Todo.js
@@ -1,34 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const passport = require('passport');
-require('../middleware/passport')(passport)
+const passport = require("passport");
+require("../middleware/passport")(passport);
 
-const todoController = require('../controller/todoController');
+const todoController = require("../controller/todoController");
 
+router.post(
+  "/Todo",
+  passport.authenticate("jwt", {
+    session: false,
+  }),
+  (req, res) => {
+    todoController.addTodo(req, res);
+  }
+);
 
-router.post('/Todo',passport.authenticate('jwt', { 
-    session: false
-}),(req, res)=>{    
-    res.send('ok')
-});
+router.get(
+  "/Todo",
+  passport.authenticate("jwt", {
+    session: false,
+  }),
+  (req, res) => {
+    todoController.getAllTodo(req, res);
+  }
+);
 
-router.get('/Todo',passport.authenticate('jwt', { 
-    session: false
-}),(req, res)=>{
-    res.send('get all')
-})
+router.put(
+  "/Todo",
+  passport.authenticate("jwt", {
+    session: false,
+  }),
+  (req, res) => {
+    todoController.editTodo(req, res);
+  }
+);
 
-router.put('/Todo',passport.authenticate('jwt', { 
-    session: false
-}),(req, res)=>{
-    res.send('edit todo')
-})
-
-router.delete('/Todo',passport.authenticate('jwt', { 
-    session: false
-}),(req, res)=>{
-    res.send('delete todo')
-})
+router.delete(
+  "/Todo",
+  passport.authenticate("jwt", {
+    session: false,
+  }),
+  (req, res) => {
+    todoController.deleteTodo(req, res);
+  }
+);
 
 module.exports = router;
