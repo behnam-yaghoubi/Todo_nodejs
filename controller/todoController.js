caa044462dfd898dc76e3fed397de4198611628d
behnam.yaghoubi <behnam.yaghoubi@partdp.ai>
2020-05-13 11:57:37 +0430

    refactor userController

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,37 +1,110 @@
-const User = require('../models/UserModel');
+const User = require("../models/UserModel");
+const Todo = require("../models/TodoModel");
 
 module.exports = {
-    async addTodo(req, res){
-        try {
-            
-        } catch (error) {
-            res.status(500).send({
-                error: `An error has occured ${error}`
-            })
-        }
-    },
-    async editTodo(req, res){
-        try {
-        } catch (error) {
-            res.status(500).send({
-                error: `An error has occured ${error}`
-            })
-        }
-    },
-    async deleteTodo(req, res){
-        try {
-        } catch (error) {
-            res.status(500).send({
-                error: `An error has occured ${error}`
-            })
-        }
-    },
-    async getAllTodo(req, res){
-        try {
-        } catch (error) {
-            res.status(500).send({
-                error: `An error has occured ${error}`
-            })
-        }
+  async addTodo(req, res) {
+    try {
+      const { todoName } = req.body;
+      const { id } = req.user;
+
+      const result = await Todo.findOrCreate({
+        where: {
+          todoName,
+          userId: id,
+        },
+      });
+      let find = result[0]._options.isNewRecord;
+      if (!find) {
+        return res.status(200).send({
+          success: true,
+          msg: "این فعالیت قبلا وارد شده است",
+          result: {},
+        });
+      } else {
+        return res.status(201).send({
+          success: true,
+          msg: "فعالیت با موفقیت افزوده شد",
+          result: result[0],
+        });
+      }
+    } catch (error) {
+      res.status(500).send({
+        error: `An error has occured ${error}`,
+      });
     }
-}
+  },
+  async editTodo(req, res) {
+    try {
+      const { id, todoName, Condition } = req.body;
+      const [code, result] = await Todo.update(
+        { todoName, Condition },
+        {
+          where: { id },
+          returning: true,
+        }
+      );
+      if (code === 0) {
+        return res.status(400).send({
+          success: "error",
+          msg: "درخواست شما پاسخی ندارد",
+          result,
+        });
+      }
+      res.status(200).send({
+        status: "success",
+        msg: "درخواست شما با موفقیت انجام شد",
+        result,
+      });
+    } catch (error) {
+      res.status(500).send({
+        error: `An error has occured ${error}`,
+      });
+    }
+  },
+  async deleteTodo(req, res) {
+    try {
+      const { id } = req.query;
+      await Todo.destroy({
+        where: {
+          id,
+        },
+      });
+      res.ststus(200).send({
+        status: "success",
+        msg: "درخواست با موفقیت انجام شد",
+      });
+    } catch (error) {
+      res.status(500).send({
+        error: `An error has occured ${error}`,
+      });
+    }
+  },
+  async getAllTodo(req, res) {
+    try {
+      const { page } = req.query;
+      const result = await Todo.findAll({
+        limit: 4,
+        offset: (page - 1) * 4,
+        where: {
+          userId: req.user.id,
+        },
+      });
+      if (!result) {
+        return res.status().send({
+          status: "error",
+          msg: "اطلاعاتی یافت نشد",
+          result,
+        });
+      }
+      res.status(200).send({
+        status: "success",
+        msg: "لیست تمام فعالیت ها",
+        result,
+      });
+    } catch (error) {
+      res.status(500).send({
+        error: `An error has occured ${error}`,
+      });
+    }
+  },
+};
--- controller/userController.js
+++ controller/userController.js
@@ -1,60 +1,69 @@
-const User = require('../models/UserModel');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require('../config/config')
+const User = require("../models/UserModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
 module.exports = {
-    async registerController(req, res){
-        try {
-            const result =await User.create({
-                userName: req.body.userName,
-                email: req.body.email,
-                password: req.body.password
-            });
-            res.status(201).json(result);
-        } catch (error) {
-            res.status(500).send({
-                error: `An error has occured ${error}`
-            })
-        }
-    },
-    async loginController(req, res){
-        try {
-            const user = await User.findOne({
-                where:{
-                    email: req.body.email
-                }
-            })
-            if (!user) {
-                    return res.status(401).send({
-                        success: false,
-                        msg: 'کاربری با این مشخصات وجود ندارد'
-                    });
-            }else{
-                        await bcrypt.compare(req.body.password, user.password,(err, isMatch)=>{
-                            if (isMatch && !err) {
-                                let userEmail = user.email;
-                                const token = jwt.sign(user.toJSON(), config.secret, {
-                                    expiresIn: '1h'
-                                })
-                                return  res.status(200).json({
-                                    success: true,
-                                    token: `jwt ${token}`,
-                                    msg : 'شما با موفقیت وارد شدید.',
-                                    redirect:'/Todo'
-                                })
-                            }else{
-                                res.status(401).send({
-                                    success: false,
-                                    msg: 'پسورد اشتباه است'
-                                });
-                            }
-                        });
-            }
-            
-        } catch (error) {
-            res.status(500).send({
-                error: `An error has occured ${error}`
-            })
-        }
+  async registerController(req, res) {
+    try {
+      const { userName, email, password } = req.body;
+      const result = await User.create({
+        userName,
+        email,
+        password,
+      });
+      res.status(201).send({
+        status: "success",
+        msg: "ثبت نام شما انجام شد",
+        result,
+      });
+    } catch (error) {
+      res.status(500).send({
+        error: `An error has occured ${error}`,
+      });
     }
-}
+  },
+  async loginController(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({
+        where: {
+          email,
+        },
+      });
+      if (!user) {
+        return res.status(401).send({
+          status: "error",
+          token: {},
+          msg: "کاربری با این مشخصات وجود ندارد",
+          redirect: "/login",
+        });
+      } else {
+        await bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (isMatch && !err) {
+            let userEmail = user.email;
+            const token = jwt.sign(user.toJSON(), config.secret, {
+              expiresIn: "1h",
+            });
+            return res.status(200).send({
+              status: "success",
+              token: `jwt ${token}`,
+              msg: "شما با موفقیت وارد شدید.",
+              redirect: "/Todo",
+            });
+          } else {
+            res.status(401).send({
+              status: "error",
+              token: {},
+              msg: "پسورد اشتباه است",
+              redirect: "/login",
+            });
+          }
+        });
+      }
+    } catch (error) {
+      res.status(500).send({
+        error: `An error has occured ${error}`,
+      });
+    }
+  },
+};
