b5e5ddb0516294d39ef2888d24f4d91ef0c566cb
behnam.yaghoubi <behnam.yaghoubi@partdp.ai>
2020-05-16 11:04:59 +0430

    add joi validation to user model

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,5 +1,6 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/database";
+import Joi from "@hapi/joi";
 const bcrypt = require("bcryptjs");
 
 export const User = sequelize.define(
@@ -32,24 +33,42 @@ export const User = sequelize.define(
   {
     // timestamps:true,
     freezeTableName: true,
+    hooks: {
+      beforeCreate: async (user, option) => {
+        // crypt user password
+        ///////////////////////////////////////////// sol 1
+        // return bcrypt.hash(user.password , 10)
+        //     .then(hash =>{
+        //         user.password = hash
+        //     })
+        //     .catch(err => {
+        //         throw new Error();
+        //     })
+        ///////////////////////////////////////////// sol 2
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+        /////////////////////////////////////////////
+      },
+    },
   }
 );
-
-User.beforeCreate(async (user, option) => {
-  // crypt user password
-  ///////////////////////////////////////////// sol 1
-  // return bcrypt.hash(user.password , 10)
-  //     .then(hash =>{
-  //         user.password = hash
-  //     })
-  //     .catch(err => {
-  //         throw new Error();
-  //     })
-  ///////////////////////////////////////////// sol 2
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
-  /////////////////////////////////////////////
-});
+export const Validate = (model) => {
+  const schema = Joi.object({
+    userName: Joi.string().required().min(5).messages({
+      "string.empty": "فیلد نام کاربری نمی تواند خالی باشد",
+      "string.min": "حداقل طول نام کاربر 5 کاراکتر است",
+    }),
+    email: Joi.string().required().email().messages({
+      "string.empty": "فیلد ایمیل نمی تواند خالی باشد",
+      "string.email": "فرمت ایمیل وارد شده صحیح نیست",
+    }),
+    password: Joi.string().required().min(6).messages({
+      "string.empty": "فیلد پسورد نمی تواند خالی باشد",
+      "string.min": "پسورد نباید کمتر از 6 کاراکتر باشد",
+    }),
+  }).options({ abortEarly: false });
+  return schema.validate(model);
+};
 
 User.prototype.validPassword = async (password) => {
   return await bcrypt.compare(password, this.password);
